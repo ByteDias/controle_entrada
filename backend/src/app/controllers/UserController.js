af58956b54bbedeb8a9f4e28d79160107dbe73a5
Lucas Matos Dias <lucasdiasdeveloper@gmail.com>
2025-09-20 11:41:53 +0000

    Repaginação do backend

---
+++ backend/src/app/controllers/UserController.js
@@ -0,0 +1,9 @@
+import UserRepository from "../repositors/UserRepository.js"
+
+class UserController {
+
+
+}
+
+// padrão Singleton
+export default new UserController()
---
+++ backend/src/app/repositors/UserRepository.js
@@ -0,0 +1,24 @@
+import { consult } from "../database/conexao.js"
+
+class UserRepository{
+    // CRUD
+    create(usuario){
+    
+    }
+    findAll(){
+       
+    }
+
+    findById(id){
+        
+    }
+    updadeById(selecao, id){
+      
+    }
+    deleteById(id){
+     
+    }
+
+}
+
+export default new UserRepository;
